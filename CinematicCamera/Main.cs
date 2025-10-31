cd6d7e59bbd339ea026c64fbe85307eef8753cd4
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2025-10-31 12:33:02 -0400

    Initial commit

---
+++ CinematicCamera/Main.cs
@@ -0,0 +1,9 @@
+﻿using SRML;
+using SRML.SR;
+
+namespace CinematicCamera;
+
+internal sealed class Main : ModEntryPoint
+{
+    public override void PreLoad() => HarmonyInstance.PatchAll(typeof(Main).Assembly);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cinematic Camera
+Ever felt the need to show the beauty of Slime Rancher? Well now you can!
